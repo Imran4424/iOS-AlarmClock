93e249e2eb4019f7663c583bd68277913b63b018
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2025-04-19 14:08:14 +0600

    project init

---
+++ AlarmClock/AlarmClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmClockApp.swift
+//  AlarmClock
+//
+//  Created by Shah Md Imran Hossain on 19/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AlarmClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlarmClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlarmClock
+//
+//  Created by Shah Md Imran Hossain on 19/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
