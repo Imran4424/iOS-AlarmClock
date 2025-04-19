9c4b1703b14f3fe8297a3d388ee790cc42d32f82
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2025-04-19 15:31:33 +0600

    add alarm clock view

---
+++ AlarmClock/View/AlarmClockView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmClockView.swift
+//  AlarmClock
+//
+//  Created by Shah Md Imran Hossain on 19/4/25.
+//
+
+import SwiftUI
+
+struct AlarmClockView: View {
+    var body: some View {
+        Text("Alarm Clock")
+    }
+}
+
+#Preview {
+    AlarmClockView()
+}
--- AlarmClock/View/ContentView.swift
+++ AlarmClock/View/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             TabView {
-                Text("Alarm")
+                AlarmClockView()
                     .tabItem {
                         Image(systemName: "alarm")
                         Text("Alarm")
