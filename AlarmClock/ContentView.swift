19963da0ef57e0694c40c3976db21156fa5306a1
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2025-04-19 15:18:05 +0600

    add content view

--- AlarmClock/ContentView.swift
+++ AlarmClock/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            TabView {
+                Text("Alarm")
+                    .tabItem {
+                        Image(systemName: "alarm")
+                        Text("Alarm")
+                    }
+                
+                Text("Clock")
+                    .tabItem {
+                        Image(systemName: "clock")
+                        Text("Clock")
+                    }
+                
+                Text("Stop Watch")
+                    .tabItem {
+                        Image(systemName: "stopwatch")
+                        Text("StopWatch")
+                    }
+                
+                Text("Timer")
+                    .tabItem {
+                        Image(systemName: "timer")
+                        Text("Timer")
+                    }
+            }
         }
-        .padding()
     }
 }
 
