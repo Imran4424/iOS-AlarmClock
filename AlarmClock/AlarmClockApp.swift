a24c8af41dc273ade99e392dc1998a490876bb3e
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2025-04-20 15:48:12 +0600

    add environment variable for alarmclock

--- AlarmClock/AlarmClockApp.swift
+++ AlarmClock/AlarmClockApp.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct AlarmClockApp: App {
+    // MARK: - Since, we are doing background fetching. Initializing here
+    @StateObject var alarmModel = AlarmModel()
+    
+    // MARK: - Scene Phase
+    @Environment(\.scenePhase) var scenePhase
+    
+    // MARK: - storing last time stamp
+    @State var lastActiveTimeStamp: Date = Date()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(alarmModel)
+        }
+        .onChange(of: scenePhase) { oldValue, newValue in
+            if newValue == .background {
+                lastActiveTimeStamp = Date()
+            } else if newValue == .active {
+                let currentTimeStampDiff = Date().timeIntervalSince(lastActiveTimeStamp)
+            }
         }
     }
 }
